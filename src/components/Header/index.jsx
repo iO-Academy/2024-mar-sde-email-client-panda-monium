dbb9425caa9dd7bb4888acb5ea5ce89fc974d25e
Kate <kate.wileman@hotmail.co.uk>
2024-04-29 11:40:01 +0100

    nav bar html

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ const Header = () => {
 return(
     <div className="flex bg-gray-700 text-white items-center w-screen justify-between p-6">
         <h1>Email Client</h1>
-        <img src="" />
+        <img src="" alt="user icon image" />
         <h4>User Name</h4>
     </div>
 
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+    return(
+        <div>
+            <h3>New Email</h3>
+            <h3>Inbox</h3>
+            <h3>Sent</h3>
+            <h3>Deleted</h3>
+        </div>
+    
+    )    
+    }
+    
+    export default NavBar
