5ed9c688d4af66faf8b4694e71209c2381b30e12
Rich <richowen87@icloud.com>
2024-04-30 11:34:57 +0100

    zero progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Inbox from "./components/Inbox";
-import InboxDetails from './components/InboxDetails'
-import NavBar from './components/NavBar'
-import Search from './components/Search'
+import { useState, useEffect } from "react"
+import "./App.css"
+import Header from "./components/Header"
+import Inbox from "./components/Inbox"
+import InboxDetails from "./components/InboxDetails"
+import NavBar from "./components/NavBar"
+import Search from "./components/Search"
 
 function App() {
-  const [emailData, setEmailData] = useState([]);
+  const [emailData, setEmailData] = useState([])
 
   useEffect(() => {
     fetch("https://email-client-api.dev.io-academy.uk/emails")
       .then((response) => response.json())
       .then((data) => {
-        setEmailData(data.data);
-      });
-  }, []);
+        setEmailData(data.data)
+      })
+  }, [])
 
   return (
     <>
       <Header />
-    <div className="flex w-screen">
-      <NavBar />
-      <div className="p-4 border w-2/12 min-w-10">
-      <Search />
-      <div className="overflow-y-auto max-h-screen">
-          {emailData.map((email) => (
-        <Inbox
-          name={email.name}
-          date={email.date_created}
-          subject={email.subject}
-          body={email.body}
-          key={email.id}
-          read={email.read}
-        />))}
+      <div className="flex w-screen">
+        <NavBar />
+        <div className="p-4 border w-2/12 min-w-10">
+          <Search />
+          <div className="overflow-y-auto max-h-screen">
+            {emailData.map((email) => (
+              <Inbox
+                name={email.name}
+                date={email.date_created}
+                subject={email.subject}
+                body={email.body}
+                key={email.id}
+                read={email.read}
+              />
+            ))}
+          </div>
         </div>
+        <InboxDetails
+          name="name1"
+          date_created="date1"
+          email="email1"
+          subject="subject1"
+          body="lorem ipsum......"
+        />
       </div>
-      <InboxDetails
-      name="name1"
-      date_created="date1"
-      email="email1"
-      subject="subject1"
-      body="lorem ipsum......"
-      />
-    </div>
-      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Inbox/index.jsx
+++ src/components/Inbox/index.jsx
@@ -1,33 +1,31 @@
-const Inbox = ({name, date, subject, body, read}) => {
+const Inbox = ({ name, date, subject, body, read }) => {
+  function reverseDate(dateString) {
+    return dateString.split("-").reverse().join("/")
+  }
 
-    function reverseDate(dateString) { 
-        return dateString.split('-').reverse().join('/'); 
-      }
-
-return (
+  return (
     <div className="">
-    {(read === "0") ? 
-    
+      {read === "0" ? (
         <div className="border-solid border-2 border-white bg-gray-600 text-white p-2">
-            <div className="flex justify-between text-sm">
-                <h3>{name}</h3>
-                <h3>{reverseDate(date.slice(0,10))}</h3>
-            </div>
-            <p className="text-xs">{subject}</p>
-            <p className="text-xs">{body}</p>
+          <div className="flex justify-between text-sm">
+            <h3>{name}</h3>
+            <h3>{reverseDate(date.slice(0, 10))}</h3>
+          </div>
+          <p className="text-xs">{subject}</p>
+          <p className="text-xs">{body}</p>
         </div>
-:
+      ) : (
         <div className="border-solid border-2 border-gray-300 bg-gray-100 text-black p-2">
-            <div className="flex justify-between text-sm">
-                <h3>{name}</h3>
-                <h3>{reverseDate(date.slice(0,10))}</h3>
-            </div>
-            <p className="text-xs">{subject}</p>
-            <p className="text-xs">{body}</p>
+          <div className="flex justify-between text-sm">
+            <h3>{name}</h3>
+            <h3>{reverseDate(date.slice(0, 10))}</h3>
+          </div>
+          <p className="text-xs">{subject}</p>
+          <p className="text-xs">{body}</p>
         </div>
-        }
+      )}
     </div>
-)    
+  )
 }
 
 export default Inbox
