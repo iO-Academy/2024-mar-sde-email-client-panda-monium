b19c168ab1b2d8a893b2082e67792a302b0df9a7
Kate <kate.wileman@hotmail.co.uk>
2024-05-01 11:54:04 +0100

    story 1 final draft 2 for review

--- src/components/EmailItem/index.jsx
+++ src/components/EmailItem/index.jsx
@@ -1,25 +1,21 @@
 const EmailItem = ({ name, date, subject, body, read, id }) => {
+  let readStatus = ""
+  if (read === "0") {
+    readStatus = " border-white bg-gray-600 text-white"
+  } else {
+    readStatus = " border-gray-300 bg-gray-100 text-black"
+  }
+
   return (
     <div>
-      {read === "0" ? (
-        <div className="border-solid border-2 border-white bg-gray-600 text-white p-2">
-          <div id={id} className="flex justify-between text-sm">
-            <h3>{name}</h3>
-            <h3>{date}</h3>
-          </div>
-          <p className="text-xs">{subject}</p>
-          <p className="text-xs">{body}</p>
+      <div className={`border-solid border-2 p-2 ${readStatus}`}>
+        <div id={id} className="flex justify-between text-sm">
+          <h3>{name}</h3>
+          <h3>{date}</h3>
         </div>
-      ) : (
-        <div className="border-solid border-2 border-gray-300 bg-gray-100 text-black p-2">
-          <div id={id} className="flex justify-between text-sm">
-            <h3>{name}</h3>
-            <h3>{date}</h3>
-          </div>
-          <p className="text-xs">{subject}</p>
-          <p className="text-xs">{body}</p>
-        </div>
-      )}
+        <p className="text-xs">{subject}</p>
+        <p className="text-xs">{body}</p>
+      </div>
     </div>
   )
 }
